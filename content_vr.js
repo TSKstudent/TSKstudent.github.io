240e35e07947edc46aad60f250c70ec4498cc088
TSKstudent <tang_shao_kai@s2023.ssts.edu.sg>
2025-12-27 11:53:06 +0800

    Add files via upload

---
+++ content_vr.js
@@ -0,0 +1,11 @@
+const DATA = {
+    research: [
+        "Don’t use your phone in a dark room...", // (Your full VR text here)
+        "Virtual reality may seem like a sophisticated..."
+        // ... etc
+    ],
+    sources: [
+        { text: "BCC Research...", link: "https://..." }
+        // ... etc
+    ]
+};
